ae2f4a26e810cd0c96cf7009f66c26a370a3ad3d
Matt Keys <MatthewjKeys22@gmail.com>
2024-12-20 16:15:53 -0500

    bascic comps

---
+++ src/components/bag.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bag: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, this is Bag!</h1>
+    </div>
+  );
+};
+
+export default Bag;
---
+++ src/components/collection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Collection: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, this is Collection!</h1>
+    </div>
+  );
+};
+
+export default Collection;
