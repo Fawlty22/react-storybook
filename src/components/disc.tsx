96bf59aaeea7755902fc913042864ba6620be372
Matt Keys <MatthewjKeys22@gmail.com>
2024-12-26 16:32:41 -0500

    hidden buttons til hover

--- src/components/disc.tsx
+++ src/components/disc.tsx
@@ -8,16 +8,16 @@ import {
   Button,
   CardActions,
 } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import IconButton from "@mui/material/IconButton";
 import { HexColorPicker } from "react-colorful";
-import Popover from '@mui/material/Popover';
+import Popover from "@mui/material/Popover";
 import { updateDisc } from "../services/disc.service";
-import PaletteIcon from '@mui/icons-material/Palette';
+import PaletteIcon from "@mui/icons-material/Palette";
 
 interface DiscProps {
-    disc: DiscDto;
-    bagToggler: (id: number, disc: DiscDto) => Promise<void>
+  disc: DiscDto;
+  bagToggler: (id: number, disc: DiscDto) => Promise<void>;
 }
 
 const Disc: React.FC<DiscProps> = ({ disc, bagToggler }) => {
@@ -40,12 +40,12 @@ const Disc: React.FC<DiscProps> = ({ disc, bagToggler }) => {
   const open = Boolean(anchorEl);
 
   const handleClick = async () => {
-      const updatedDisc = { ...disc, inBag: !disc.inBag };
-      await bagToggler(disc.id, updatedDisc); 
-    };
+    const updatedDisc = { ...disc, inBag: !disc.inBag };
+    await bagToggler(disc.id, updatedDisc);
+  };
 
   return (
-      <Card
+    <Card
       variant="outlined"
       style={{ height: "100%" }}
       sx={{
@@ -54,83 +54,102 @@ const Disc: React.FC<DiscProps> = ({ disc, bagToggler }) => {
           transform: "scale(1.03)",
           boxShadow: 6,
           border: "1px solid dodgerBlue",
-          "& .cart-icon": {
+          "& .hidden-button": {
             opacity: 1,
-          }
+          },
         },
       }}
     >
       <CardContent>
-        <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
-          <div style={{display: 'flex'}}>
-            <Typography variant="h6" component="div" sx={{marginRight: '6px'}}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ marginRight: "6px" }}
+            >
               {disc.name}
             </Typography>
-            <IconButton 
+            <IconButton
               onClick={handleColorClick}
               sx={{
                 backgroundColor: color,
-                width: '32px',
-                height: '32px',
-                '&:hover': {
+                width: "32px",
+                height: "32px",
+                "&:hover": {
                   backgroundColor: color,
-                  opacity: 0.8
-                }
+                  opacity: 0.8,
+                },
               }}
             >
-              {!color && <PaletteIcon/>}
+              {!color && <PaletteIcon />}
             </IconButton>
             <Popover
               open={open}
               anchorEl={anchorEl}
               onClose={handleClose}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
+                vertical: "bottom",
+                horizontal: "center",
               }}
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
+                vertical: "top",
+                horizontal: "center",
               }}
-              sx={{marginTop: '6px'}}
+              sx={{ marginTop: "6px" }}
             >
               <HexColorPicker color={color} onChange={handleColorChange} />
             </Popover>
           </div>
-        <Chip
-          label={disc.inBag ? "In Bag" : "Not in Bag"}
-          color={disc.inBag ? "primary" : "secondary"}
-        />
+          <Chip
+            label={disc.inBag ? "In Bag" : "Not in Bag"}
+            color={disc.inBag ? "primary" : "secondary"}
+          />
         </div>
-        
+
         <Typography variant="body2" color="textSecondary">
           {disc.brand}
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          Speed: {disc.speed} | Glide: {disc.glide} | Turn: {disc.turn}{" "}
-          | Fade: {disc.fade}
+          Speed: {disc.speed} | Glide: {disc.glide} | Turn: {disc.turn} | Fade:{" "}
+          {disc.fade}
         </Typography>
-        
       </CardContent>
-      <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
-        <Button size="small" variant="contained" color={disc.inBag ? "primary" : "secondary"} onClick={handleClick}>
-        {disc.inBag ? 'Remove from Bag' : 'Add to Bag'}
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Button
+          sx={{
+            opacity: 0,
+            transition: "opacity 0.3s ease",
+          }}
+          size="small"
+          variant="contained"
+          color={disc.inBag ? "primary" : "secondary"}
+          onClick={handleClick}
+          className="hidden-button"
+        >
+          {disc.inBag ? "Remove from Bag" : "Add to Bag"}
         </Button>
-        <IconButton 
-          className="cart-icon"
-          aria-label="Add to Cart" 
-          href={`https://infinitediscs.com/${disc.brand}-${disc.name}`} 
+        <IconButton
+          className="hidden-button"
+          aria-label="Add to Cart"
+          href={`https://infinitediscs.com/${disc.brand}-${disc.name}`}
           target="_blank"
           sx={{
             opacity: 0,
-            transition: 'opacity 0.3s ease'
+            transition: "opacity 0.3s ease",
           }}
         >
           <ShoppingCartIcon />
         </IconButton>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
 
 export default Disc;
